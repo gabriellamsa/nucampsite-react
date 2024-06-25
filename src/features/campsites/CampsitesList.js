409967ad871551859017a23afcd55d601886502e
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-06-25 19:02:11 +0100

    campsitesSlice

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
+  const campsites = selectAllCampsites();
+
   return (
     <Row className='ms-auto'>
-      {CAMPSITES.map((campsite) => {
+      {campsites.map((campsite) => {
         return (
           <Col md='5' className='m-4' key={campsite.id}>
             <CampsiteCard campsite={campsite} />
@@ -15,6 +17,6 @@ const CampsitesList = () => {
       })}
     </Row>
   );
-}
+};
 
 export default CampsitesList;
