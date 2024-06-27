e86d9210c8cd68340a3adc240fc0fc85fefbcb0e
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-06-27 15:20:27 +0100

    Comments

--- src/features/campsites/CampsiteDetail.js
+++ src/features/campsites/CampsiteDetail.js
@@ -4,7 +4,7 @@ const CampsiteDetail = ( {campsite} ) => {
   const { image, name, description } = campsite;
 
   return (
-    <Col md='12' className='m-4'>
+    <Col md='' className='m-'>
       <Card>
         <CardImg top src={image} alt={name} />
         <CardBody>
