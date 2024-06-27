85238c48abee3f0924ee25718ae6fd289d293d16
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-06-27 14:57:54 +0100

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -5,15 +5,29 @@ import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
 import './App.css';
+import CampsiteDetailPage from './pages/CampsiteDetailPage.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='contact' element={<ContactPage />} />
-        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route 
+          path='/' 
+          element={<HomePage />} 
+        />
+        <Route 
+          path='contact' 
+          element={<ContactPage />} 
+        />
+        <Route 
+          path='directory' 
+          element={<CampsitesDirectoryPage />} 
+        />
+        <Route 
+          path='directory/:campsiteId' 
+          element={<CampsiteDetailPage />} 
+        />
       </Routes>
       <Footer />
     </div>
