56caa31ecd59d8be6cbb33ed825e6629bf9bdc11
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-07-02 15:35:40 +0100

    Redux for All

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Col } from 'reactstrap';
+import { useSelector } from "react-redux";
 import Partner from "./Partner";
 import { selectAllPartners } from './partnersSlice';
 
 const PartnerList = () => {
-  const partners = selectAllPartners();
+  const partners = useSelector(selectAllPartners);
 
   return (
     <Col className='mt-4'>
