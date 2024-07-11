35642781b39f1a8d805fcf2a09b41bcfeab4f087
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-07-11 18:30:52 +0100

    Async Comments

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,8 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { COMMENTS } from '../../app/shared/COMMENTS';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../app/shared/baseUrl';
+// import { COMMENTS } from '../../app/shared/COMMENTS';
+
+export const fetchComments = createAsyncThunk(
+  'comments/fetchComments',
+  async () => {
+    const response = await fetch(baseUrl + 'comments');
+    if (!response.ok) {
+      return Promise.reject('Unable to fetch, status: ' + response.status);
+    }
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-  commentsArray: COMMENTS
+  commentsArray: [],
+  isLoading: true,
+  errMsg: ''
 };
 
 const commentsSlice = createSlice({
@@ -18,13 +33,25 @@ const commentsSlice = createSlice({
         };
         state.commentsArray.push(newComment);
       }
+    },
+    extraReducers: {
+      [fetchComments.pending]: (state) => {
+        state.isLoading = true;
+      },
+      [fetchComments.fulfilled]: (state, action) => {
+        state.isLoading = false;
+        state.errMsg = '';
+        state.commentsArray = action.payload;
+      },
+      [fetchComments.rejected]: (state, action) => {
+        state.isLoading = false;
+        state.errMsg = action.error ? action.error.message : 'Fetch failed';
+      }
     }
 });
 
 export const commentsReducer = commentsSlice.reducer;
-
 export const { addComment } = commentsSlice.actions;
-
 export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
   return state.comments.commentsArray.filter(
     (comment) => comment.campsiteId === parseInt(campsiteId)
